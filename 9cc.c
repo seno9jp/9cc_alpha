0f59a0d3518c6bbc6c779f35ba8d7151a4f89bb2
senokazu <seno.kazu@gmail.com>
2021-01-11 15:59:34 +0900

    パーサ追加

--- 9cc.c
+++ 9cc.c
@@ -5,6 +5,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+// -----------------------------------------------
+// トークナイザー
+// -----------------------------------------------
+
+
 // トークンの種類
 typedef enum {
     TK_RESERVED, // 記号
@@ -123,6 +128,89 @@ Token *tokenize() {
 
     new_token(TK_EOF, cur, p);
     return head.next;
+}
+
+
+// ------------------------------------------------
+// パーサ
+// ------------------------------------------------
+
+// 抽象構文木のノードの種類
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+    NodeKind kind; // ノードの型
+    Node *lhs;     // 左辺 left-hand side
+    Node *rhs;     // 右辺 right-hand side
+    int val;       // kindがND_NUMの場合のみ使う
+};
+
+Node *new_node(NodeKind kind, Node *lhs, *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *mul();
+Node *expr();
+Node *primary();
+
+Node *expr() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+        } else if (consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *mul() {
+    Node *node = primary();
+
+    for (;;) {
+        if (consume('*')) {
+            node = new_node(ND_MUL, node, primary());
+        } else if (consume('/')) {
+            node = new_node(ND_DIV, node, primary());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *primary() {
+    // 次のトークンが"("なら、"(" expr ")"のはず
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    // そうでなければ数値のはず
+    return new_node_num(expect_number());
 }
 
 int main(int argc, char **argv) {
